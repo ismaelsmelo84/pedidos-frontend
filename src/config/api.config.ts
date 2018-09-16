a0d2c489178629a4628f365759b6e1578ee7a0c6
Ismael Melo <ismaelsmelo84@gmail.com>
2018-09-16 12:14:08 -0300

    Aplicação-base ajustada para frontend de pedidos

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,3 +2,6 @@ export const API_CONFIG = {
     baseUrl: "https://spring-ionic-nelio.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
 }
+//baseUrl: "http://localhost:8080",
+
+//bucketBaseUrl: "assets/image"
